028293e617792d2947999972ef3af03863a9de91
mugenfugafuga@outlook.jp <mugenfugafuga@outlook.jp>
2020-05-03 21:25:38 +0900

    zip2

---
+++ Mugen/Mugen.Tests/Enumerable2Tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Mugen.Tests
+{
+    [TestClass()]
+    public class Enumerable2Tests
+    {
+        [TestMethod()]
+        public void Enumerable2_Zip2Test()
+        {
+            var v1 = Enumerable.Range(0, 100);
+            var v2 = Enumerable.Range(10, 10).Select(i => i.ToString());
+
+            var i = 0;
+
+            foreach (var vv in v1.Zip2(v2))
+            {
+                Assert.AreEqual(i, vv.Item1);
+                Assert.AreEqual((i + 10).ToString(), vv.Item2);
+                ++i;
+            }
+
+            Assert.AreEqual(10, i);
+
+        }
+
+        [TestMethod()]
+        public void Enumerable2_Zip2Test1()
+        {
+            var v1 = Enumerable.Range(0, 100);
+            var v2 = Enumerable.Range(10, 10).Select(i => i.ToString());
+            var v3 = Enumerable.Range(100, 5).Select(i => i.ToString());
+
+            var i = 0;
+
+            foreach (var vv in v1.Zip2(v2, v3))
+            {
+                Assert.AreEqual(i, vv.Item1);
+                Assert.AreEqual((i + 10).ToString(), vv.Item2);
+                Assert.AreEqual((i + 100).ToString(), vv.Item3);
+                ++i;
+            }
+
+            Assert.AreEqual(5, i);
+        }
+    }
+}
---
+++ Mugen/Mugen/Enumerable2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mugen
+{
+    public static class Enumerable2
+    {
+        public static IEnumerable<TResult> Zip2<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
+            => first.Zip(second, resultSelector);
+        public static IEnumerable<Tuple<TFirst, TSecond>> Zip2<TFirst, TSecond>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second)
+            => Zip2(first, second, Tuple.Create);
+        public static IEnumerable<TResult> Zip2<TFirst, TSecond, TThird, TResult>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second, IEnumerable<TThird> third, Func<TFirst, TSecond, TThird, TResult> resultSelector)
+        {
+            var e1 = first.GetEnumerator();
+            var e2 = second.GetEnumerator();
+            var e3 = third.GetEnumerator();
+
+            while(e1.MoveNext() && e2.MoveNext() && e3.MoveNext())
+            {
+                yield return resultSelector(e1.Current, e2.Current, e3.Current);
+            }
+        }
+        public static IEnumerable<Tuple<TFirst, TSecond, TThird>> Zip2<TFirst, TSecond, TThird>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second, IEnumerable<TThird> third)
+            => Zip2(first, second, third, Tuple.Create);
+    }
+
+}
