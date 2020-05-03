80c7fcc9dcd4a048a4b5986895600e4f3fa7e009
mugenfugafuga@outlook.jp <mugenfugafuga@outlook.jp>
2020-05-03 21:08:02 +0900

    atomic

---
+++ Mugen/Mugen.Tests/Threading/MAtomicTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mugen.Threading.Tests
+{
+    [TestClass()]
+    public class MAtomicTests
+    {
+        [TestMethod()]
+        public void MAtomic_MAtomicTest()
+        {
+            var hoge = new Hoge();
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(10);
+                hoge.Value = "atomic!";
+            });
+
+            Assert.AreEqual(0, hoge.Status);
+
+
+            while (hoge.Status == 0)
+            {
+                Thread.Sleep(1);
+            }
+
+            Assert.AreEqual(1, hoge.Status);
+            Assert.AreEqual("atomic!", hoge.Value);
+        }
+
+        class Hoge
+        {
+            private MReaderWriterLock rwlock;
+            private MAtomic<int> status;
+            private MAtomic<string> val;
+
+            public Hoge()
+            {
+                rwlock = new MReaderWriterLock();
+                status = new MAtomic<int>(rwlock, 0);
+                val = new MAtomic<string>(rwlock); 
+            }
+
+            public int Status => status;
+
+            public string Value
+            {
+                get => val;
+                set
+                {
+                    using var wlock = rwlock.GetWriterLock();
+
+                    status.ValueNoLock = 1;
+                    val.ValueNoLock = value;
+                }
+            }
+        }
+    }
+}
---
+++ Mugen/Mugen/Threading/MAtomic.cs
@@ -0,0 +1,44 @@
+﻿namespace Mugen.Threading
+{
+    public class MAtomic<T>
+    {
+        private MReaderWriterLock rwLock;
+
+        public T ValueNoLock { get; set; }
+
+
+        public MAtomic(MReaderWriterLock rwLock, T value)
+        {
+            this.rwLock = rwLock;
+            this.ValueNoLock = value;
+        }
+
+        public MAtomic(MReaderWriterLock rwLock) : this(rwLock, default(T))
+        {
+        }
+
+        public MAtomic(T value) : this(new MReaderWriterLock(), value)
+        {
+        }
+
+        public MAtomic() : this(new MReaderWriterLock(), default(T))
+        {
+        }
+
+        public T Value
+        {
+            get
+            {
+                using var rlock = rwLock.GetReaderLock();
+                return ValueNoLock;
+            }
+            set
+            {
+                using var wlock = rwLock.GetWriterLock();
+                ValueNoLock = value;
+            }
+        }
+
+        public static implicit operator T(MAtomic<T> atomic) => atomic.Value; 
+    }
+}
